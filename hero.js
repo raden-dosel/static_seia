4f97162d8a2158407bd3ea8a61310bb9f7270a55
Raden Cedrick  Dosel <raden.dosel00@gmail.com>
2025-10-15 14:18:50 +0000

    Add final touches on the design

--- hero.js
+++ hero.js
@@ -9,8 +9,8 @@
 				<h1 class="hero__title">SEIA Security Installation Services</h1>
 				<p class="hero__subtitle">Expert electric fence installations and automatic gate systems for ultimate protection and convenience.</p>
 				<div class="hero__actions">
-					<a class="hero__btn" href="#features">Get started</a>
-					<a class="hero__btn hero__btn--ghost" href="about.html">Learn more</a>
+					<a class="hero__btn" href="#portfolio">Browse Portfolio</a>
+					<a class="hero__btn hero__btn--ghost" href="#about">Learn more</a>
 				</div>
 			</div>
 		</section>`;
